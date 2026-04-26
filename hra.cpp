a8878fcd18e2f11dc7db95f83cce422e0dab6ba3
Jendoforo <jendicek.nemec@gmail.com>
2026-04-26 20:10:23 +0200

    Add files via upload

---
+++ hra.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+int volba;
+int postava;
+int hp;
+int atack;
+int mana;
+
+int main() {
+ cout<<"------VITEJTE V JENDOVE HRE!------";
+ cout<<endl;
+ cout<<"Jakou postavu si vyberete sefe?";
+ cout<<endl;
+ cout<<"PRVNI POSTAVA NA MEM UZASNEM SEZNAMU:";
+ cout<<endl;
+ cout<<"(1)ADAM RYBA!! Staty:";
+ cout<<endl;
+ cout<<"Zivoty 5";
+ cout<<endl;
+ cout<< "Utok 4";
+ cout<<endl;
+ cout<< "(2) ASSASIN!! Staty:";
+ cout<< " Zivoty 3";
+ cout<< endl;
+ cout<< "Utok 5 ";
+
+
+
+    return 0;
+}
